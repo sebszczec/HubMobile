e46df8daeec5a583be0429bb5c5f3cf47b5e2363
sebszczec@wp.pl <sebszczec@wp.pl>
2018-07-30 20:17:52 +0200

    Some playing with button

--- app/src/main/java/com/example/sebsz/hubmobile/MainActivity.java
+++ app/src/main/java/com/example/sebsz/hubmobile/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sebsz.hubmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v)
+    {
+        Button b = (Button)v;
+        b.setText(getString(R.string.ButtonText2));
     }
 }
